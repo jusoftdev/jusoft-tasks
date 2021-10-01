f9bd26edb4576f529e944c50873f2cb5a21afc71
juhom205 <80626044+juhom205@users.noreply.github.com>
2021-10-01 13:46:37 +0200

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -4,14 +4,7 @@ import "firebase/auth"
 import "firebase/database"
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCeVw1HmIXBAL7wNi_eANHjiDp27_1NKiY",
-  authDomain: "jusoft-tasks.firebaseapp.com",
-  databaseURL: "https://jusoft-tasks-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "jusoft-tasks",
-  storageBucket: "jusoft-tasks.appspot.com",
-  messagingSenderId: "955398270578",
-  appId: "1:955398270578:web:b08da184d9e202f71e1ceb",
-  measurementId: "G-SY9WZV4TW6"
+  //Firebase Config
 }
 
 let firebaseApp = firebase.initializeApp(firebaseConfig)
